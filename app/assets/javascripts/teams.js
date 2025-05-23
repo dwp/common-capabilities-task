01976c610f97b1015af73a4c5b3f0a63375623ef
Ash-h-dwp <ash.henry@engineering.digital.dwp.gov.uk>
2025-05-23 10:03:31 +0100

    update team creation prototype
    GitOrigin-RevId: b2d0c56b9fd35478a067776e8a9e0bd1b40977f1

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -3,20 +3,107 @@ console.log("teams js")
 const teamsAddMemberButton = document.getElementById("addTeamMember")
 const searchValue = document.getElementById("searchb")
 const member = localStorage.getItem("member");
-const searchc = document.getElementsByClassName('autocomplete__input')[0]
+const searchb = document.getElementsByClassName('autocomplete__input')[0]
+//const searchc = document.getElementsByClassName('autocomplete__input')[1]
+//const searchc = document.getElementById('searchc')
 const TeamMembers = []
 const teamTableBody = document.getElementById('TeamMembersTable')
 const tablePlaceholder = document.getElementById('tablePlaceholder')
 const TeamMembersList = document.getElementById('TeamMembersList')
+const TeamName = document.getElementById('team-name');
+const TeamDesc = document.getElementById('team-desc');
+const CreateTeamButton = document.getElementById('create-team-button')
 const STML = JSON.parse(localStorage.getItem("teamMembers"));
+
+const TeamDetailsList = document.getElementById('team-details-list');
+
+
+if(TeamDetailsList){
+  const details = JSON.parse(localStorage.getItem("teamDetails"))
+console.log(details.name)
+  let html =`
+     
+          <div class="govuk-summary-list__row">
+            <dt class="govuk-summary-list__key">
+              Name
+            </dt>
+            <dd class="govuk-summary-list__value">
+             ${details.name}
+            </dd>
+            <dd class="govuk-summary-list__actions">
+              <a class="govuk-link" href="#">Change<span class="govuk-visually-hidden"> date of birth</span></a>
+            </dd>
+          </div>
+          <div class="govuk-summary-list__row">
+            <dt class="govuk-summary-list__key">
+              Description
+            </dt>
+            <dd class="govuk-summary-list__value">
+            ${details.desc}
+            </dd>
+            <dd class="govuk-summary-list__actions">
+              <a class="govuk-link" href="#">Change<span class="govuk-visually-hidden"> date of birth</span></a>
+            </dd>
+          </div>
+          <div class="govuk-summary-list__row">
+            <dt class="govuk-summary-list__key">
+              Function
+            </dt>
+            <dd class="govuk-summary-list__value">
+            ${details.tf}
+            </dd>
+            <dd class="govuk-summary-list__actions">
+              <a class="govuk-link" href="#">Change<span class="govuk-visually-hidden"> date of birth</span></a>
+            </dd>
+          </div>
+          <div class="govuk-summary-list__row">
+            <dt class="govuk-summary-list__key">
+              Team Lead
+            </dt>
+            <dd class="govuk-summary-list__value">
+            ${details.tln}
+            </dd>
+            <dd class="govuk-summary-list__actions">
+              <a class="govuk-link" href="#">Change<span class="govuk-visually-hidden"> date of birth</span></a>
+            </dd>
+          </div>
+      
+  `
+  TeamDetailsList.innerHTML = html
+}
+
+if(CreateTeamButton) {
+const tln = document.querySelectorAll('#searchb')
+const fn = document.querySelectorAll('#searchc')
+  CreateTeamButton.addEventListener("click", function(e){
+    //e.preventDefault();
+    const teamName = TeamName.value
+    const teamDesc = TeamDesc.value
+    const teamLeader = tln[0].childNodes[0]._component.state.query;
+    const teamFunction = fn[0].childNodes[0]._component.state.query;
+   // console.log("clicked", teamFunction, teamLeader )
+
+    let team = {
+      name : teamName,
+      desc : teamDesc,
+      tln : teamLeader,
+      tf :  teamFunction
+    }
+
+    //console.log(team)
+    localStorage.setItem("teamDetails", JSON.stringify(team));
+  })
+}
 
 if(teamsAddMemberButton){
 
 teamsAddMemberButton.addEventListener("click", function(e){
     e.preventDefault()
-    const memberDetails = searchb[0].childNodes[0]._component.state.query;
+    let tm = document.querySelectorAll('#searchb')
+    console.log(tm[0].childNodes[0]._component.state.query, "y")
+    const memberDetails = tm[0].childNodes[0]._component.state.query;
     createTeamMemberObject(memberDetails)
-    searchb[0].value="";
+    //searchb[0].value="";
 
     
 })
@@ -29,7 +116,7 @@ if(TeamMembersList) {
         <tr class="govuk-table__row">
     <td class="govuk-table__cell">${x.name}</td>
     <td class="govuk-table__cell">${x.staffNo}</td>
-    <td class="govuk-table__cell"><a class="govuk-link" href="#">Remove</a></td>
+    <td class="govuk-table__cell govuk-!-text-align-right"><a class="govuk-link " href="#">Remove</a></td>
 
   </tr>`
         
@@ -80,4 +167,9 @@ function createTeamMemberObject(data){
    
 
 
+}
+
+function storeTeamDetails(){
+
+  localStorage.setItem("teamDetails", JSON.stringify(Teamdetails));
 }
