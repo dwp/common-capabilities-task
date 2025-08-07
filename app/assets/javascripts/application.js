f6832e34378f3b03023b3ddb6cff1010f7b12611
Ash Henry <ash.henry@engineering.digital.dwp.gov.uk>
2025-08-07 13:49:21 +0100

    teams view
    GitOrigin-RevId: 4730af7e0c54c3212fd04cde31becc9cd7553152

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -3,20 +3,20 @@
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
 
-window.GOVUKPrototypeKit.documentReady(() => {
-  // Add JavaScript here
-  var rows = document.getElementsByClassName("govuk-table__row");
-  console.log(rows);
-  Array.prototype.forEach.call(
-    rows,
-    (row) => {
-      row.addEventListener("mouseover", (event) => {
-        console.log(event)
-        row.classList.add("grey-row");
-      })
-      row.addEventListener("mouseout", (event) => {
-        row.classList.remove("grey-row");
-      })
-    }
-  )
-})
+// window.GOVUKPrototypeKit.documentReady(() => {
+//   // Add JavaScript here
+//   var rows = document.getElementsByClassName("govuk-table__row");
+//   console.log(rows);
+//   Array.prototype.forEach.call(
+//     rows,
+//     (row) => {
+//       row.addEventListener("mouseover", (event) => {
+//         console.log(event)
+//         row.classList.add("grey-row");
+//       })
+//       row.addEventListener("mouseout", (event) => {
+//         row.classList.remove("grey-row");
+//       })
+//     }
+//   )
+// })
