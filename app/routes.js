ac24ddf981daa666f318f7cdf2f79721ef9567bc
Ash Henry <ash.henry@engineering.digital.dwp.gov.uk>
2025-10-22 10:17:04 +0100

    stats-21
    GitOrigin-RevId: ddf28d543b83fc15dbec5a00102c0cd57423d072

--- app/routes.js
+++ app/routes.js
@@ -10,6 +10,22 @@ const fakeCaseData = require('./data/generate-case-data')
 
 // Add your routes here
 
+// Routes for bulk change
+// it is posting to itself and after redirected to v1 or v2
+router.post('/teams/bulk-changes/select-version', (req, res) => {
+	if (req.session.data.teams.selectVersion == 'Version 1'){
+		res.redirect("version-1/home")
+	} else {
+		res.redirect("version-2/home")
+	}
+})
+
+
+router.post('/teams/bulk-changes/version-1/view-and-manage-tasks', (req, res) => {
+		res.redirect("results")
+})
+
+
 const express = require('express')
 const dayjs = require('dayjs')
 var objectSupport = require("dayjs/plugin/objectSupport");
