4a8eedf2a2f360c490f7f13b2ce5af9cbeac3ced
Kirill.Titov1 <kirill.titov@engineering.digital.dwp.gov.uk>
2025-11-18 16:55:20 +0000

    View and manage tasks journey added
    GitOrigin-RevId: cf54e458cfe719cc3f748c015137e21e90972f2b

--- app/routes.js
+++ app/routes.js
@@ -87,5 +87,6 @@ router.post('/test/date-test-answer', function (req, res) {
 
 module.exports = router
 
+require('./routes/view-and-manage-tasks/view-and-manage-tasks')(router)
 require('./routes/bulk-changes/bulk-changes-routes')(router)
 require('./routes/stats-for-agents-and-teams/stats-routes')(router)
