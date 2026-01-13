4076bdafd8bfa338aa005644dc226601186167c7
Kirill.Titov1 <kirill.titov@engineering.digital.dwp.gov.uk>
2026-01-13 12:07:48 +0000

    new prototype added
    GitOrigin-RevId: 7bbd54333e33f426b89398ccf30c12cdab883751

---
+++ app/routes/agents-attached-to-a-team/agents-attached-to-a-team-routes.js
@@ -0,0 +1,17 @@
+module.exports = router => {
+
+// Routes for bulk change
+// it is posting to itself and after redirected to v1 or v2
+
+router.post('/teams/agents-attached-to-a-team/select-user', (req, res) => {
+	if (req.session.data.teams.selectUser == 'Admin'){
+		res.redirect("admin/home")
+	} else if (req.session.data.teams.selectUser == 'Service manager') {
+		res.redirect("service-manager/home")
+	} else {
+		res.redirect("team-leader/home")
+	}
+})
+
+
+}
