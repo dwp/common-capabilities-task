f3a30231c9c5b165745caedf6c8c38310c120628
Kirill.Titov1 <kirill.titov@engineering.digital.dwp.gov.uk>
2026-02-03 17:42:51 +0000

    4419 - updated
    GitOrigin-RevId: b093928320520054e1f3352b367635db697d4894

--- app/routes/notification-when-removing-an-agent/notification-when-removing-an-agent.js
+++ app/routes/notification-when-removing-an-agent/notification-when-removing-an-agent.js
@@ -17,22 +17,27 @@ router.post('/teams/notification-when-removing-an-agent/service-manager/delete',
 	res.redirect("list-of-agents")
 })
 
+router.post('/teams/notification-when-removing-an-agent/service-manager/remove-user-v2', (req, res) => {
+	res.redirect("agents")
+})
+
+
 
 
 // POST route
-router.post('/teams/notification-when-removing-an-agent/service-manager/remove-user', (req, res) => {
-  const sm = req.session.data?.notificationWhenRemovingAgent?.sm;
+// router.post('/teams/notification-when-removing-an-agent/service-manager/remove-user', (req, res) => {
+//   const sm = req.session.data?.notificationWhenRemovingAgent?.sm;
 
-  if (sm === 'No') {
-    // Clear the flag if you don't want the banner on agent-details
-    req.session.data.agents = undefined;
-    res.redirect('agent-details');
-  } else {
-    // Set the flag so Nunjucks sees data['agents'] === "yes"
-    req.session.data.agents = 'yes';
-    res.redirect('agents');
-  }
-});
+//   if (sm === 'No') {
+//     // Clear the flag if you don't want the banner on agent-details
+//     req.session.data.agents = undefined;
+//     res.redirect('agent-details');
+//   } else {
+//     // Set the flag so Nunjucks sees data['agents'] === "yes"
+//     req.session.data.agents = 'yes';
+//     res.redirect('agents');
+//   }
+// });
 
 
 }
