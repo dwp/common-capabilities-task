4d318054f9b149a00ea3dae3e0c3de5f1ab79f1e
Kirill.Titov1 <kirill.titov@engineering.digital.dwp.gov.uk>
2026-02-05 15:00:56 +0000

    Team - Notification when removind an agent from a team who has tasks assigned - 4436-2
    GitOrigin-RevId: 5d6d8bb6195cf6b63d463cef89bd37f0eac550f1

--- app/routes/notification-when-removing-an-agent/notification-when-removing-an-agent.js
+++ app/routes/notification-when-removing-an-agent/notification-when-removing-an-agent.js
@@ -22,22 +22,12 @@ router.post('/teams/notification-when-removing-an-agent/service-manager/remove-u
 })
 
 
+// team-notification-when-removind-an-agent-from-a-team
 
+router.post('/teams/team-notification-when-removind-an-agent-from-a-team/service-manager/remove-agent-01', (req, res) => {
+	res.redirect("manage-agents")
+})
 
-// POST route
-// router.post('/teams/notification-when-removing-an-agent/service-manager/remove-user', (req, res) => {
-//   const sm = req.session.data?.notificationWhenRemovingAgent?.sm;
-
-//   if (sm === 'No') {
-//     // Clear the flag if you don't want the banner on agent-details
-//     req.session.data.agents = undefined;
-//     res.redirect('agent-details');
-//   } else {
-//     // Set the flag so Nunjucks sees data['agents'] === "yes"
-//     req.session.data.agents = 'yes';
-//     res.redirect('agents');
-//   }
-// });
 
 
 }
