3324aa9ba9881198fddd6a02e75306661778e9f5
Kirill Titov <kirill.titov@engineering.digital.dwp.gov.uk>
2026-04-27 12:47:50 +0100

    user profile banners
    GitOrigin-RevId: 8ad14dc9e4634b061112267aca610cccfd1f65e8

--- app/routes/agent-profile/agent-profile.js
+++ app/routes/agent-profile/agent-profile.js
@@ -3,9 +3,6 @@ module.exports = router => {
 // Routes for bulk change
 // it is posting to itself and after redirected to v1 or v2
 
-router.post('/spikes/agent-profile/service-manager-v2/manage-org-unit/change-attributes', (req, res) => {
-	res.redirect("agent-details-sm")
-})
 
 // Team
 
@@ -19,22 +16,22 @@ router.post('/spikes/agent-profile/team-leader/agent-details-mt', (req, res) => 
 
 // End Team
 
+// SM
+
 router.post('/spikes/agent-profile/service-manager-v2/manage-org-unit/change-groups', (req, res) => {
 	res.redirect("agent-details-sm")
 })
 
 
-router.post('/agent-details-mt', function (req, res) {
-	req.session.data.showBanner = req.body.showBanner
-res.redirect('/agent-details-mt')
+router.post('/spikes/agent-profile/service-manager-v2/manage-org-unit/agent-details-sm', (req, res) => {
+	res.redirect("manage-agents")
 })
 
+// Matching criteria
 
-router.post('/agent-details-sm', function (req, res) {
-	req.session.data.showBanner = req.body.showBanner
-res.redirect('/agent-details-sm')
+router.post('/spikes/agent-profile/service-manager-v2/manage-org-unit/change-attributes', (req, res) => {
+	res.redirect("agent-details-sm")
 })
-
 
 
 }
